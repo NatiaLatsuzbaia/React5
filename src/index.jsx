056b76c1f7b36b60e1a64b11502e4556c1f1cd66
NatiaLatsuzbaia <125747976+NatiaLatsuzbaia@users.noreply.github.com>
2024-01-31 23:31:55 +0400

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+class Natia extends React.Component{
+    constructor (){
+        super();
+        
+        this.state =  {
+            name: " ",
+        };
+    }
+
+    setValue(){
+        this.setState({
+          name: "demna",
+        });
+        window.alert(this.state.name);
+        
+    }
+    render() {
+        
+        return (
+            <React.Fragment>
+                {
+                <button type="button" class="btn btn-warning CCC" onClick={() => this.setValue()}>დააჭირე</button>
+                }
+
+            </React.Fragment>
+        )
+    }
+}
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Natia />);
